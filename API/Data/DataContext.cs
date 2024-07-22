2b515986703eab90826179e06e016966bcbf1bff
thandoVsoft <162423460+thandoVsoft@users.noreply.github.com>
2024-07-22 15:17:20 +0200

    End of section 4

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -10,5 +10,5 @@ public class DataContext : DbContext
     {
     }
 
-public DbSet<AppUser> Users { get; set; }
+    public DbSet<AppUser> Users { get; set; }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,16 +1,21 @@
+using System.CommandLine.Parsing;
+using System.Text;
 using API.Data;
+using API.Extentions;
+using API.interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddCors();
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
+
 
 var app = builder.Build();
 
@@ -18,6 +23,8 @@ var app = builder.Build();
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
